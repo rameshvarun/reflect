a0e31312ba9d5ebf6e4a564eb523f80ab658ae84
Varun Ramesh <varun.ramesh@aol.com>
2015-03-22 02:21:22 -0700

    Removed sword.

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -13,28 +13,15 @@ public class PlayerScript : MonoBehaviour {
 
     public Transform shield;
     public Transform attackBounds;
-    public Transform sword;
     public Transform playerSprite;
-    
-    public bool shieldMode;
-
-    // Variables for handling attacking 
-    private bool attacking;
-    private Vector3 attackDirection;
-    private float attackTime;
 
 	// Use this for initialization
 	void Start () {
         controller = GetComponent<CharacterController>(); // Character Controller
-        attacking = false; // Start out not attacking
 
         // Player Sprite Shadows
         playerSprite.GetComponent<SpriteRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
         playerSprite.GetComponent<SpriteRenderer>().receiveShadows = true;
-
-        // Sword Sprite Shadows
-        sword.GetComponentInChildren<SpriteRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
-        sword.GetComponentInChildren<SpriteRenderer>().receiveShadows = true;
 	}
 
     Vector3 lastAim;
@@ -56,7 +43,7 @@ public class PlayerScript : MonoBehaviour {
     /// <param name="distance">How far from the player the bullet was when it was attacked.</param>
     /// <returns>The position to place the bullet at for the next grame.</returns>
     public Vector3 bulletPosition(float distance) {
-        return transform.position + attackDirection.normalized * distance;
+        return transform.position + lastAim.normalized * distance;
     }
 	
 	// Update is called once per frame
@@ -65,8 +52,6 @@ public class PlayerScript : MonoBehaviour {
         Vector3 aim = new Vector3(Input.GetAxis("AttackHorizontal"), 0, Input.GetAxis("AttackVertical"));
         float trigger = Input.GetAxis("Attack");
 
-        if (Input.GetKeyDown(KeyCode.Space)) shieldMode = !shieldMode;
-
         // Mouse controls
         if (Input.GetMouseButton(1)) {
             Vector3 diff = new Vector3(Input.mousePosition.x - Screen.width/2, 0, Input.mousePosition.y - Screen.height / 2);
@@ -74,64 +59,23 @@ public class PlayerScript : MonoBehaviour {
             if (Input.GetMouseButton(0)) trigger = 1.0f;
         }
 
-        if (attacking) movement = Vector3.zero; // Can't move while attacking
         float speed = (aim.magnitude > 0.2) ? walkSpeed : runSpeed;
         controller.SimpleMove(speed * movement);
 
-        if (shieldMode) {
-            if (aim.magnitude > 0.2) {
-                aim.Normalize();
-                shield.gameObject.SetActive(true);
-                shield.transform.localPosition = shieldDistance * aim;
+        if (aim.magnitude > 0.2) {
+            aim.Normalize();
+            shield.gameObject.SetActive(true);
+            shield.transform.localPosition = shieldDistance * aim;
 
-                float angleBetween = Vector3.Angle(new Vector3(0, 0, 1), aim);
-                if (aim.x < 0) angleBetween = -angleBetween;
+            float angleBetween = Vector3.Angle(new Vector3(0, 0, 1), aim);
+            if (aim.x < 0) angleBetween = -angleBetween;
 
-                Quaternion rotation = Quaternion.AngleAxis(angleBetween, Vector3.up);
-                shield.transform.rotation = rotation;
+            Quaternion rotation = Quaternion.AngleAxis(angleBetween, Vector3.up);
+            shield.transform.rotation = rotation;
                 
-            }
-            else {
-                shield.gameObject.SetActive(false);
-            }
         }
         else {
             shield.gameObject.SetActive(false);
-
-            if (aim.magnitude > 0.2 || attacking) {
-                sword.gameObject.SetActive(true);
-
-                if (!attacking) {
-                    sword.localPosition = swordDistance * aim.normalized;
-                    sword.rotation = Quaternion.LookRotation(aim.normalized);
-                }
-            } else {
-                sword.gameObject.SetActive(false);
-            }
-
-            if (aim.magnitude > 0.2 && Mathf.Abs(trigger) > 0.2 && Mathf.Abs(lastTrigger) < 0.2) {
-                attacking = true;
-                attackTime = 0.2f;
-                attackDirection = aim.normalized;
-            }
-        }
-
-        if (attacking) {
-            attackBounds.gameObject.SetActive(true); // Draw the attack bounds
-
-            attackBounds.transform.localPosition = attackDistance * attackDirection; // Set attack bounds to right in fornt of player
-
-            // Rotate attack bounds to be coming out of player
-            float angleBetween = Vector3.Angle(new Vector3(0, 0, 1), attackDirection);
-            if (attackDirection.x < 0) angleBetween = -angleBetween;
-            Quaternion rotation = Quaternion.AngleAxis(angleBetween, Vector3.up);
-            attackBounds.transform.rotation = rotation;
-
-            // End of attack time
-            attackTime -= Time.deltaTime;
-            if (attackTime < 0) attacking = false;
-        } else {
-            attackBounds.gameObject.SetActive(false); // Don't draw attack bounds
         }
 
         // Keep track of Input data from the previous frame
